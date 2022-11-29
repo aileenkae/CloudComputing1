ffe94339b0dd24e11cb735d782393b84f6da16be
Blerona16 <blerona_b@hotmail.com>
2022-11-29 17:37:49 +0100

    add forms in body

--- frontend/src/components/body.js
+++ frontend/src/components/body.js
@@ -1,30 +1,37 @@
 import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import "./body.css"
+import Delete from '@mui/icons-material/DeleteForeverOutlined';
+import AddIcon from '@mui/icons-material/Add';
 
 
 function Body() {
   return (
-    <div className='template'>
-        <AppBar position="static" style={{backgroundColor:'#f4f4f9', paddingBottom: 400}}>
-             <span style={{fontSize:40, color:"#202124", marginLeft: 800}}> Start new Form </span>
-          
-
-        <Box
-          sx={{
-          display: 'flex',  flexWrap: 'wrap',
-           '& > :not(style)': {   m: 1, width: 128, height: 128,        },
-                }}
-         >
-         <Paper elevation={2}>Form1 </Paper>
-         <Paper elevation={2}>Form2 </Paper>
-         <Paper elevation={2}>Form3 </Paper>
-        </Box>
-        </AppBar>
+    <div className='mainbody' style={{backgroundColor: '#f4f4f9' , paddingBottom: '250px', paddingTop: '250px' }} > 
+      <div className='box'>      
+        <div >
+          <span style={{fontSize:40, color:"#202124", alignItems: 'center'}}> Your Forms </span>
+        </div>
+                
+        
+        <button className='forms'> 
+        <p>Create New Form</p>
+        <AddIcon style={{backgroundColor: '#aadab9'}}> </AddIcon>
+        </button>
+        
+        <button className='forms'>   
+        <p>Form #1</p>
+        <Delete style={{color: "black", padding:'3px', marginRight: '3px', borderRadius: '2px'}}></Delete>
+              
+        </button>
+        <button className='forms'> 
+        <p>Form #2</p>
+          <Delete style={{color: "black", padding:'3px', marginRight: '3px', borderRadius: '2px'}}></Delete>
+        </button>
+        
+        </div>
               
     </div>
-
+        
   );
   
 }
