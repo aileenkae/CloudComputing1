c21004fab0182c37f34d446704078f6fd7891c07
Blerona16 <blerona_b@hotmail.com>
2022-11-30 21:37:25 +0100

    change landing page

--- frontend/src/components/body.js
+++ frontend/src/components/body.js
@@ -1,34 +1,42 @@
 import React from 'react'
-import "./body.css"
 import Delete from '@mui/icons-material/DeleteForeverOutlined';
 import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 
+function ResponsiveAppBarBODY() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-function Body() {
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+{/*function Body() { */}
   return (
-    <div className='mainbody' style={{backgroundColor: '#f4f4f9' , paddingBottom: '250px', paddingTop: '250px' }} > 
-      <div className='box'>      
-        <div >
-          <span style={{fontSize:40, color:"#202124", alignItems: 'center'}}> Your Forms </span>
-        </div>
-                
-        
-        <button className='forms'> 
-        <p>Create New Form</p>
-        <AddIcon style={{backgroundColor: '#aadab9'}}> </AddIcon>
+    <div style={{backgroundColor: '#f4f4f9' , paddingBottom: '200px', paddingTop: '150px' }} > 
+      <Typography style={{fontSize:50, fontWeight:700, color:'#aadab8', alignContent:'center', marginLeft:700, marginBottom:70 }}> Your Forms </Typography>  
+      
+      <div style={{ marginLeft:200,  marginRight: 200, display:'flex' , flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }} > 
+
+        <button style={{height: '250px', width: '300px', cursor:'pointer'}}> 
+        <p style={{fontSize: 20}}>Create New Form</p>
+        <AddIcon style={{fontSize:150, color:'#aadab8' }}> </AddIcon>
         </button>
         
-        <button className='forms'>   
-        <p>Form #1</p>
-        <Delete style={{color: "black", padding:'3px', marginRight: '3px', borderRadius: '2px'}}></Delete>
-              
+        <button style={{height: '250px', width: '300px', cursor:'pointer'}}>   
+        <p style={{fontSize: 20}}>Form #1</p>
+        <IconButton onClick={handleOpenNavMenu}  size="large" aria-label="account of current user"  aria-controls="menu-appbar" aria-haspopup="true" color="inherit" style={{marginTop:100, marginLeft:200}} >  
+        <Delete style={{fontSize: 30, borderRadius: '2px'}} /> </IconButton>
         </button>
-        <button className='forms'> 
-        <p>Form #2</p>
-          <Delete style={{color: "black", padding:'3px', marginRight: '3px', borderRadius: '2px'}}></Delete>
+
+        <button style={{height: '250px', width: '300px', cursor:'pointer'}}> 
+        <p style={{fontSize: 20}}>Form #2</p>
+        <IconButton onClick={handleOpenNavMenu}  size="large" aria-label="account of current user"  aria-controls="menu-appbar" aria-haspopup="true" color="inherit" style={{marginTop:100, marginLeft:200}} >  
+        <Delete style={{fontSize: 30, borderRadius: '2px'}} /> </IconButton>
         </button>
         
-        </div>
+      </div>
               
     </div>
         
@@ -36,4 +44,4 @@ function Body() {
   
 }
 
-export default Body;
+export default ResponsiveAppBarBODY;
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,19 +1,19 @@
 import * as React from 'react';
-import './header.css'
-import AppBar from '@mui/material/AppBar';
+//import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+//import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
+//import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 //import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Description';
 import SearchIcon from '@mui/icons-material/Search';
+
 
 //const pages = ['Home', 'Formular'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -38,79 +38,39 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <div className='header' style={{ backgroundColor:'#AADAB8', maxWidth:'xl'  }} >
-          <AdbIcon className='header' style={{ display:'flex', fontSize: "3rem", color:'#f4f4f9'}} />
-          {/* Logo Text */}
-          <Typography className='header' variant="h6"  component="a"   href="/"
-            sx={{ fontWeight: 700, letterSpacing: '.3rem',  color: '#f4f4f9',  textDecoration: 'none' }}   >
-            Form
-          </Typography>
+    <div style={{position:'sticky', padding: 5,  display:'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor:'#AADAB8', maxWidth:'xl'}} >
+      <AdbIcon style={{ display:'flex', marginLeft: 10 , fontSize: "3rem", color:'#f4f4f9' }} />
+          Logo
+      <Typography variant="h4"  component="a"   href="/"
+          style={{ fontWeight: 700, letterSpacing: '.2rem',  color: '#f4f4f9',  textDecoration: 'none' }}   >
+          Forms
+      </Typography>
 
-          <div className="header" style={{ display:'flex', flexDirection:'row', width: 700, height: 45, alignItems:'center', backgroundColor: '#f4f4f9', borderRadius:5}}>
-            <IconButton ><SearchIcon  /></IconButton>
-            <input type="text" name="search" placeholder='Search' style={{border:'none', height:40, background:'transperant', outline:'none', widh:'100%' }} />
-          </div>
-              
+          
+      <div style={{position:'sticky', display:'flex', flexDirection:'row', width: 400, height: 45, alignItems:'center', backgroundColor: '#f4f4f9', borderRadius:5}}> 
+        <IconButton ><SearchIcon /></IconButton>
+        <input type="text" name="search" placeholder='Search' style={{border:'none', height:40, background:'transparent', outline:'none', widh:'100%'}} />
+      </div>
+          
+  
+      <IconButton onClick={handleOpenNavMenu}  size="large" aria-label="account of current user"  aria-controls="menu-appbar" aria-haspopup="true" color="inherit" > 
+       <MenuIcon />  </IconButton>
 
-          <Box className='header' sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {/*pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))*/}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-
-            {/* Menu Text */}  
-          </Typography>
-          {<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {/*pages.map((page) => (
+       <Menu id="menu-appbar"  anchorEl={anchorElNav} anchorOrigin={{vertical: 'bottom',horizontal: 'left',}}
+          keepMounted
+          transformOrigin={{vertical: 'top',horizontal: 'left',}}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{ display: { xs: 'block', md: 'none' }}} >
+          {/*pages.map((page) => (
+            <MenuItem key={page} onClick={handleCloseNavMenu}>
+              <Typography textAlign="center">{page}</Typography>
+            </MenuItem>
+          )) */}
+        </Menu>
+        
+      {/*  {<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -118,39 +78,29 @@ function ResponsiveAppBar() {
               >
                 {page}
               </Button>
-            ))*/}
-          </Box> }
+            ))
+          </Box> } */}
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+         <Box sx={{ flexGrow: 0 }}>
+         <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
+            
+            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{vertical: 'top',horizontal: 'right'}}
+              keepMounted   transformOrigin={{ vertical: 'top', horizontal: 'right' }}
               open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              onClose={handleCloseUserMenu}> 
+                {settings.map((setting) => (
+                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                 </MenuItem>  ))}
             </Menu>
           </Box>
-        
+            
+         
+         
       
     </div>
   );
