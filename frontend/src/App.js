f5be116954698503d3f8472fc5d7de6fdee6754a
Aileen Kästle <ak277@hdm-stuttgart.de>
2022-12-02 14:45:05 +0100

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,21 +22,7 @@ function App() {
 
 
   return (
-    <div>
-      <ResponsiveAppBar/>
-      {/*(typeof backendData.test === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        backendData.test.map((tests,i) => (
-          <p key={i}>{tests}</p>
-        ))
-        )*/}
 
-      <BottomNavigation/>
-      <Body />
-      <Question/>
-
-    </div>
   )
 }
 
