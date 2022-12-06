ead82903f8f73e35efbbc8bee0fc7890a475d364
Aileen Kästle <ak277@hdm-stuttgart.de>
2022-12-06 15:45:09 +0100

    add form_site path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React /*, {useEffect, useState}*/ from 'react';
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import {Home} from './components/Home_Site';
 import {Login} from './components/login_site';
+import {Form} from './components/Form_Site';
 
 
 function App() {
@@ -27,7 +28,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home/>}/> 
         <Route path="/login" element={<Login/>}/>
-        <Route path="/form" element={<h1>Formular</h1>}/>
+        <Route path="/form" element={<Form/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Form_Site.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Question from './question';
+
+export function Form() {
+  return (
+    <Question/>
+  );
+}
+
+export default Form
