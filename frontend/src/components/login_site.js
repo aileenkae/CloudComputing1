ab4aaf4ed6a65a080e06d00f834bbed3737be611
Aileen Kästle <ak277@hdm-stuttgart.de>
2022-12-06 16:53:23 +0100

    add login to login_site

--- frontend/src/components/login_site.js
+++ frontend/src/components/login_site.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import AdbIcon from '@mui/icons-material/Description';
 import Typography from '@mui/material/Typography';
+import Authenticate from './authenticate';
+import { Link } from 'react-router-dom';
 
 export function Login() {
   return (
-
+    <div>
     <div style={{position:'sticky', padding: 5,  display:'flex', alignItems: 'center', backgroundColor:'#AADAB8', maxWidth:'xl'}} >
-      <AdbIcon style={{ display:'flex', marginLeft: 10 , fontSize: "3rem", color:'#f4f4f9' }} />
+    <Link to='/home'><AdbIcon style={{ display:'flex', marginLeft: 10 , fontSize: "3rem", color:'#f4f4f9' }} /></Link>
       <Typography variant="h4"  component="a"   href="/"
           style={{ fontWeight: 700, letterSpacing: '.2rem',  color: '#f4f4f9',  textDecoration: 'none' }}   >
           Forms   Login
       </Typography>
 
 
+    </div>
+      <div style={{position: 'absolut', marginLeft: '45%', marginTop: '100px'}}>
+        <Authenticate />
+      </div>
     </div>
 
 
