a443aff7bc8083f1229f26c63ac197cd1779917a
Aileen Kästle <ak277@hdm-stuttgart.de>
2022-12-14 16:17:45 +0100

    add frage name in json

--- frontend/src/components/question.js
+++ frontend/src/components/question.js
@@ -7,6 +7,7 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Fade from "@mui/material/Fade";
 import Question_ra from "./quest-r";
+import uuid from 'react-uuid'
 
 
 function Question() {
