5f2c90a92a78ac1ed4df676592dfeaab76e33fcc
Blerona16 <blerona_b@hotmail.com>
2023-01-15 16:18:41 +0100

    add comments

--- frontend/src/layouts/unauthorised.jsx
+++ frontend/src/layouts/unauthorised.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// UnauthorisedLayout component provides a layout for pages that don't require user to be logged in
+// It renders passed children within a container with a gray background
+
 export default function UnauthorisedLayout(props) {
     return (
         <div className = "bg-gray-100 min-w-screen min-h-screen flex items-top justify-center py-8">
